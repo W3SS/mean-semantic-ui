eb0e20261f6facd5f7e560e8f2ba58e938920e3d
Foysal Ahamed <cfaion341@gmail.com>
2015-01-08 18:21:46 +0100

    working draft with the latest semantic-ui

---
+++ config/env/all.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+	app: {
+		title: 'mean-semantic-ui',
+		description: 'Mean.js boilerplate with semantic ui instead of twbs',
+		keywords: 'MongoDB, Express, AngularJS, Node.js, Semantic UI'
+	},
+	port: process.env.PORT || 3000,
+	templateEngine: 'swig',
+	sessionSecret: 'MEAN',
+	sessionCollection: 'sessions',
+	assets: {
+		lib: {
+			css: [
+				'public/lib/semantic-ui/dist/semantic.min.css',
+			],
+			js: [
+				'public/lib/angular/angular.js',
+				'public/lib/angular-resource/angular-resource.js', 
+				'public/lib/angular-cookies/angular-cookies.js', 
+				'public/lib/angular-animate/angular-animate.js', 
+				'public/lib/angular-touch/angular-touch.js', 
+				'public/lib/angular-sanitize/angular-sanitize.js', 
+				'public/lib/angular-ui-router/release/angular-ui-router.js',
+				'public/lib/angular-ui-utils/ui-utils.js',
+				'public/lib/jquery/dist/jquery.min.js',
+				'public/lib/semantic-ui/dist/semantic.min.js'
+			]
+		},
+		css: [
+			'public/modules/**/css/*.css'
+		],
+		js: [
+			'public/config.js',
+			'public/application.js',
+			'public/modules/*/*.js',
+			'public/modules/*/*[!tests]*/*.js'
+		],
+		tests: [
+			'public/lib/angular-mocks/angular-mocks.js',
+			'public/modules/*/tests/*.js'
+		]
+	}
+};
